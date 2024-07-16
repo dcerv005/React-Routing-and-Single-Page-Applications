e53f061c7e4ab6f9a9ab2600f1390ff6ed1b040c
dcerv005 <dacervantes12@gmail.com>
2024-07-16 16:20:15 -0700

    CharacterDetails.jsx fix

--- lesson-4-project/src/CharacterDetails.jsx
+++ lesson-4-project/src/CharacterDetails.jsx
@@ -1,21 +1,20 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import {number} from "prop-types"
-import {useParams, useNavigate} from 'react-router-dom';
+import {params} from "prop-types"
+// import {useParams, useNavigate} from 'react-router-dom';
 import { Link } from "react-router-dom";
 
 
 const CharacterDetails = (props) => {
     const [id, setId] = useState(props.params)
-    const [character, setCharacter]= useState([])
+    const [character, setCharacter]= useState(null)
     
     
-    console.log(id)
-    console.log(props.params)
+    
     useEffect( () => {
         if (props.params) {
-            setId(props.params)
-            console.log(id)
+            
+            
             const fetchData = async () => {
                 try {
                     const response = await axios.get(`https://gateway.marvel.com/v1/public/characters/${id.id}?&ts=1&apikey=1db16fa474c3fdf9e74d956d788ea55f&hash=e1c499a5ceb1d8ab8e4afbeb45348103`);
@@ -30,7 +29,20 @@ const CharacterDetails = (props) => {
             setId(null)
         }
     }, []);
-     
+    
+    if (!character) {
+        if (!id) {
+            return (
+                <div> 
+                    <h2>Character Details Page</h2>
+                    <p>Pick the character you want to know more information about on our <Link to='/characters'>Marvel Characters</Link> page.</p>
+                </div>
+            )
+        } else {
+            return <div> Loading... </div>
+        }    
+    }
+
     
    
 
@@ -44,19 +56,19 @@ const CharacterDetails = (props) => {
                     <li key={index}>{comic.name}</li>
                 ))}
             </ul> */}
-            {id == null && (
+            {/* {id == null && (
                 <>
                     <h2>Character Details Page</h2>
                     <p>Pick the character you want to know more information about on our <Link to='/characters'>Marvel Characters</Link> page.</p>
                 </>
-            )}
+            )} */}
             <br />
         </div>
     )
 }
 
-CharacterDetails.propType = {
-    characterId: number
-}
+// CharacterDetails.propTypes = {
+//     params: params
+// }
 
 export default CharacterDetails;
